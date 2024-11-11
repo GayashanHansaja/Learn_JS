cbac09cbd4d93d36f7d378a8a0169da6d6e909da
IT22171856 <142132191+IT22171856@users.noreply.github.com>
2024-11-11 17:06:11 +0530

    type convertion,const

--- index.js
+++ index.js
@@ -8,3 +8,30 @@ document.getElementById("button").onclick=function(){
     username=document.getElementById("input").value;
     document.getElementById("myh1").textContent=`Hello ${username}`
 }
+
+//Type convertion
+/* const PI=3.14;
+let radius;
+let circumference;
+
+PI=756.78;
+radius=Number(radius);
+
+circumference=2*PI*radius;
+
+console.log(circumference);77
+ */
+
+// check const
+
+/* const PI=3.14;
+let radius;
+let circumference;
+document.getElementById("button2").onclick=function(){
+    radius=document.getElementById("input2").value;
+    radius=Number(radius);
+    circumference=2*PI*radius;
+    document.getElementById("myh3").textContent=`Circumference of the circle is ${circumference}`
+
+
+} */
